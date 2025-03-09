52f1e49c1eff406fb87b62a8e55e82ddc16ec61d
AukTo1380 <auktoafg@gmail.com>
2025-03-09 19:58:46 +0430

    upate the user profiles

---
+++ Frontend/src/pages/UserProfilePage.jsx
@@ -0,0 +1,10 @@
+import React, { isValidElement } from "react";
+import { Button } from "@mui/material";
+function UserProfilePage() {
+  return <div>
+    User Profies 
+    
+  </div>;
+}
+
+export default UserProfilePage;
