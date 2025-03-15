f1a4ef1e95cfbb538c9bfa1497a9d67d2ac4712b
AukTo1380 <auktoafg@gmail.com>
2025-03-15 10:19:47 +0430

    test

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,7 +4,6 @@ import PrivateRoute from "./components/common/PrivateRoute";
 import OnlyAdminPrivateRoute from "./components/common/OnlyAdmin";
 import ScrollTop from "./components/common/ScrollTop";
 import Signin from "./features/authentication/components/Signin";
-import Setting from "./pages/dashboard/pages/setting";
 export default function App() {
   return (
     <div>
@@ -13,11 +12,9 @@ export default function App() {
         <Routes>
           {/* public routes all users */}
           <Route path="/sign-in" element={<Signin />} />
-          {/* <Route path="/sign-up" element={<Signup />} /> */}
-<<<<<<<<< Temporary merge branch 1
-          {/* <Route path="/userprofile" element={<UserProfilePage />} /> */}
-=========
->>>>>>>>> Temporary merge branch 2
+          <Route path="*" element={<NotFound />} />
+
+          {/* private route for users*/}
           <Route element={<PrivateRoute />}>
             <Route path="/dashboard" element={<DashboardPage />} />
           </Route>
